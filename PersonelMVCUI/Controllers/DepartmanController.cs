a1d0c19603a6941e6c653dc531a635b7b48bb38b
OmerTopaloglu <OmerTopaloglu@DESKTOP-JVCESJ4>
2021-11-08 22:19:58 +0300

    Add project files.

---
+++ PersonelMVCUI/Controllers/DepartmanController.cs
@@ -0,0 +1,35 @@
+﻿using PersonelMVCUI.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonelMVCUI.Controllers
+{
+    public class DepartmanController : Controller
+    {
+
+        PersonelDbEntities db = new PersonelDbEntities();
+
+        public ActionResult Index()
+        {
+            var model = db.Departman.ToList();
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Yeni()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Yeni(Departman departman)
+        {
+            db.Departman.Add(departman);
+            db.SaveChanges();
+            return RedirectToAction("Index","Departman");
+        }
+    }
+}
